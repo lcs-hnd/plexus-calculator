0bbbe3a6204931faebd31d06b17d2fbd6b3486f4
Lucas Honda <103671864+lcs-hnd@users.noreply.github.com>
2025-05-14 14:41:46 -0400

    Moving over some files from the previous repo.

---
+++ calculator/script.js
@@ -0,0 +1,25 @@
+const addition = (a, b) => a + b;
+
+const subtraction = (a, b) => a - b;
+
+const division = (a, b) => a / b;
+
+const multiplication = (a, b) => a * b;
+
+// operators written with arrows functions using implicit returns
+
+function operate(operand1, operand2, operator){
+    return operator(operand1, operand2);
+};
+
+function valueHolders(){
+    let operand1 = null;
+    let operand2 = null;
+    let operator = null;
+    if (null) {
+        return operate(operand1, operand2, operator);
+    }
+    else {
+        return "WIP"
+    }
+}
