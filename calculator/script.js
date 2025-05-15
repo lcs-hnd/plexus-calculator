06234d1e639a027d76f951fdf798affb4d4d58c0
Lucas Honda <103671864+lcs-hnd@users.noreply.github.com>
2025-05-15 17:22:08 -0400

    Animating with plexus. Still working through the documentation.

--- calculator/script.js
+++ calculator/script.js
@@ -38,7 +38,48 @@ calculatorButtons.forEach(btn => {
 
 // background-video
 
-const video = document.getElementById('background-video');
-video.playbackRate = 0.7;
+// const video = document.getElementById('background-video');
+// video.playbackRate = 0.7;
 
-// resolve entry sfx
+// plexus animation, leaving more comments than usual since it's my first time making one
+
+const canvas = document.getElementById('plexus'); // grabbing the html element
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d'); // grabs the 2d rendering context
+
+window.addEventListener('resize', () => { // uses window global object to resize the canvas
+    canvas.width=window.innerWidth;
+    canvas.height=window.innerHeight;
+});
+
+let particles = []; // array for the particles
+
+for (let i = 0; i < Math.floor(window.innerWidth/10); i++) { // variable particle count based on canvas width, might change it to base around both height and width
+    particles.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        vx: (Math.random() - 0.5) * 0.5, // -0.5 to 0.5 halved for random velocity
+        vy: (Math.random() - 0.5) * 0.5
+    });
+};
+
+function plexusAnimation() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clears canvas to prevent trailing
+    particles.forEach(p1 => {
+        p1.x += p1.vx; // moving particles according to their velocities
+        p1.y += p1.vy;
+
+        if (p1.x < 0 || p1.x > canvas.width) p1.vx *= -1; // checks particle position to make sure it's not off the viewport
+        if (p1.y < 0 || p1.y > canvas.height) p1.vy *= -1; // the velocity multiplier can be reduced from -1 to simulate kinetic energy loss
+
+        particles.forEach(p2 => {
+            const dx = p1.x - p2.x;
+            const dy = p1.y - p2.y;
+            const dist = Math.sqrt(dx*dx + dy*dy);
+
+        });
+
+    });
+}
